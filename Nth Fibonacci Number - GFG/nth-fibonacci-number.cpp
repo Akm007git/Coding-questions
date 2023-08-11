0f1f8b199e04333a26379d621bc65783affded03
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-08-11 21:54:07 +0530

    Added solution - LeetHub

---
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+// USING  DP MEMORIZATION top down approach 
+
+class Solution {
+    
+    int mod = 1e9+7;
+    
+    int fibbo(int n , vector<int>&dp)
+    {
+        
+        
+         
+        // base case 
+        if(n < 2)
+            return n;
+            
+        // checking in dp availbale or not
+        if(dp[n] != -1)
+            return dp[n];
+        
+        // receursive call
+        dp[n] = (fibbo((n-1),dp)%mod  + fibbo((n-2),dp)%mod )%mod;
+        return dp[n];
+        
+    }
+    
+  public:
+    int nthFibonacci(int n){
+        // code here
+        vector<int>dp(n+1 ,-1);
+        return  fibbo(n,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
