06a027749a9d67bf29d864610a84a029324e640f
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-08-12 23:54:19 +0530

    Added solution - LeetHub

--- 3 Divisors - GFG/3-divisors.cpp
+++ 3 Divisors - GFG/3-divisors.cpp
@@ -7,21 +7,24 @@ class Solution
 {
 public:
 
+// cocept of sieve of eratosthenis
+
+
   long long  isPrime(long long n)
   {
-      vector<long long>v(n+1, true);
+      vector<long long>v(n+1, true); // initilize all elemnt as true
       long long count =0;
-      for(long long i =2;i*i<=n;i++)
+      for(long long i =2;i*i<=n;i++) // limit squart of n
       {
           for(int j = i*i; j<=n;j +=i)
           {
-              v[j] = false;
+              v[j] = false; // marking false the divisable of 2, 3 , if get
           }
       }
       
-      for(long long i=2;i*i<=n;i++)
+      for(long long i=2;i*i<=n;i++) 
       {
-          if(v[i] && i*i <= n)
+          if(v[i] && i*i <= n) // checking if the elemnt true or not and if true, its square in the range or not 
           {
               count ++;
              // cout<<count<<endl;
@@ -36,8 +39,8 @@ public:
         vector<int >ans;
         for(int i=0;i<q;i++)
         {
-            long long count = isPrime(query[i]);
-            ans.push_back(count);
+            long long count = isPrime(query[i]); // each time passing single query
+            ans.push_back(count); // store in vector
             
         }
         return ans;
