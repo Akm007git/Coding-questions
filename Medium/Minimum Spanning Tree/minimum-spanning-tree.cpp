3735cc5e5cee50dc18b6181d7362c7310322f414
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-09-25 11:37:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,6 +3,74 @@
 using namespace std;
 
 // } Driver Code Ends
+
+#define vc vector<int>
+
+class disjoint
+{
+public:
+    vc parent, rank, Size; // initialize two vector
+
+    disjoint(int size)
+    {
+        rank.resize(size + 1, 0); // intialize with 0
+        parent.resize(size + 1);
+        Size.resize(size + 1, 1);
+        // parent vector initialie with ultimate parent , means itself
+        for (int i = 0; i < size; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // find ultimate parent and also path cmpression
+
+    int findUltimateParent(int node)
+    {
+        if (parent[node] == node) // parent is itself
+            return node;
+        else
+        {
+            // using dp, memorise the parent directly with each node, path compression
+            parent[node] = findUltimateParent(parent[node]);
+            return parent[node];
+        }
+    }
+
+    // by rank
+    void unionByRank(int u, int v)
+    {
+        int ultimateParent_U = findUltimateParent(u);
+        int ultimateParent_V = findUltimateParent(v);
+
+        // step1 , check both ultimate parent equal or not
+        if (ultimateParent_U == ultimateParent_V) // node in same componenet
+        {
+            return;
+        }
+        else if (rank[ultimateParent_U] < rank[ultimateParent_V])
+        {
+            parent[ultimateParent_U] = ultimateParent_V;
+            rank[ultimateParent_V]++;
+        }
+        else if (rank[ultimateParent_V] < rank[ultimateParent_U])
+        {
+
+            parent[ultimateParent_V] = ultimateParent_U;
+            rank[ultimateParent_U]++;
+        }
+        else
+        {
+
+            parent[ultimateParent_U] = ultimateParent_V;
+            rank[ultimateParent_V]++;
+        }
+    }
+};
+
+
+/*--------------MAIN CLASS -------------------*/
+
 class Solution
 {
 	public:
@@ -10,42 +78,45 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<int>visited(V,0);
-        priority_queue< pair<int,int>, vector<pair<int,int> >, greater<pair<int,int>> >pq;
+        vector<pair<int, pair<int,int> > > edges;
         
-        // distence, node
-        pq.push({0,0}); //initially distence 0, node 0;
+        for(int i=0;i<V;i++)
+        {
+            for(auto it:adj[i])
+            {
+                int adjnode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt,{node,adjnode}}); // adding i adges
+            }
+        }
+        
+        sort(edges.begin(),edges.end()); // sort for minimum size
+        disjoint ds(V);
+        
         int sum = 0;
         
-        
-        while(!pq.empty())
+        for(auto it:edges)
         {
-            int wt = pq.top().first; // first pairr wt
-            int node = pq.top().second; // second pair node
-            pq.pop();
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
             
-            if(visited[node] == 1) // if already visited continue
-                continue;
-            
-            visited[node] = 1; // mark as visited
-            sum += wt; // increase sum
-            
-            // processing the adjacnet node,and add in pq
-            
-            for(auto it:adj[node]) // it returning a pair vector adjnode,distence 
+            if(ds.findUltimateParent(u) !=  ds.findUltimateParent(v))
             {
-                int adjnode = it[0]; // value of adj node
-                int adjwt = it[1]; // weight to reach this adj node
-                
-                if(visited[adjnode] == 0) // if adjnode not visited then push into queue dist,node
-                {
-                    pq.push(make_pair(adjwt,adjnode)); 
-                }
+                sum += wt;
+                ds.unionByRank(u,v);
             }
+            
             
         }
         return sum;
         
+        
+        
+        
+
     }
 };
 
