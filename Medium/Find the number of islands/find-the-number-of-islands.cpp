5b2d09d05635b7248ed089c3659999aa586dc80b
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-09-25 13:28:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -4,76 +4,62 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    
-    
-    private:
-    void  bfs(int row, int col, vector<vector<char>> &grid, vector<vector<bool>> &visited )
-    {
-        
-        int n = grid.size();
+  public:
+  
+  void solveByDfs(int row, int col , vector<vector<bool>>&visited, vector<vector<char>>grid )
+  {
+        int n =grid.size();
         int m = grid[0].size();
         
-        queue<pair<int,int>>q ;
-        
-        // initially oush the start node
-        q.push({row,col});
-        
-        // mark as true of initial node
-        visited[row][col] = true;
-        
-        while( !q.empty())
-        {
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            
-            // now traversing and checking all thw adjacent nodes
-            
-            for(int delRow = -1;delRow<=1;delRow++)
-            {
-                for(int delCol = -1 ;delCol <=1; delCol++)
-                {
-                    int newRow = row + delRow;
-                    int newCol = col + delCol;
-                    
-                    // checking visited or not, each neighbour
-                    if( newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && 
-                        grid[newRow][newCol] == '1' && !visited[newRow][newCol] )
-                     {
-                         visited[newRow][newCol] = true;
-                         q.push({newRow,newCol});
-                     }
-                    
-                }
-            }
-        }
-        
-        
-        
-    }
-    
-  public:
+      visited[row][col] = true;
+      int drow = row;
+      int dcol = col;
+      
+      // traversing all 8 direction
+      for(drow = -1; drow <= 1;drow++)
+      {
+          for( dcol = -1; dcol <= 1;dcol++)
+          {
+              int newRow = row +drow;
+              int newCol = col + dcol;
+              
+              
+              if(newRow >=0 &&  newRow <n  && newCol >= 0 && newCol <m 
+                    && !visited[newRow][newCol] && grid[newRow][newCol] == '1' )
+              {
+                  
+                  solveByDfs(newRow,newCol,visited,grid);
+              }
+          }
+      }
+  }
+  
+  
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
         // Code here
-        int n  = grid.size();
+        int n =grid.size();
         int m = grid[0].size();
-        int island = 0;
         
-        
-        vector<vector<bool> >visited(n,vector<bool>(m,false)); // vector of vector with all false
-        for(int row = 0; row<n;row++)
+    vector<vector<bool>>visited(n,vector<bool>(m,false));
+    
+    int iSland = 0;
+    
+    
+        for(int i=0;i<n;i++)
         {
-            for(int col = 0; col<m;col++)
+            for(int j =0;j<m;j++ )
             {
-                if( !visited[row][col] && grid[row][col] == '1' )
+                if(!visited[i][j] && grid[i][j] == '1')
                 {
-                    island ++;
-                    bfs(row,col,grid,visited);
+                    solveByDfs(i,j,visited,grid);
+                    iSland++;
                 }
             }
         }
-        return island;
+    
+    return iSland++;
+        
     }
 };
 
