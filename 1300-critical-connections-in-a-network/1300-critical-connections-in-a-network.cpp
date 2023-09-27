cad1e10079dc31f9a69bfb185b5ddd0b53721663
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-09-27 15:34:33 +0530

    Time: 488 ms (39.65%), Space: 175.2 MB (48.58%) - LeetHub

---
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -0,0 +1,69 @@
+class Solution {
+
+private:
+void solveBydfs(int node, int parent,int &timing, 
+vector<int>&ins,vector<int>&low, vector<bool>&visited,vector<int>adj[],vector<vector<int>>&bridges)
+{
+
+    visited[node] = true;
+    ins[node] = low[node] = timing;
+
+    timing++;
+
+    // procesiing adj nodes
+
+    for(auto it:adj[node])
+    {
+        // if adj nodes is parent
+        if(it == parent )
+        {
+            continue;
+        }
+
+        if(!visited[it])
+        {
+            solveBydfs(it,node,timing,ins,low,visited,adj,bridges);
+
+            low[node] = min(low[it],low[node]);
+            // bridge check
+            if(low[it] > ins[node])
+            {
+                bridges.push_back({node,it}); 
+            }
+        }
+        else
+        {
+            low[node] = min(low[it],low[node]);
+        }
+    }
+
+}
+
+public:
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        
+        // creating undirected adjacentnodes
+        vector<int>adj[n];
+        for(auto it: connections)
+        {
+            int u = it[0];
+            int v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int>insTime(n);
+        vector<int>lowInsTime(n);
+
+
+        vector<vector<int>>bridges;
+        vector<bool>visited(n,false);
+        int timing = 1;
+        int src = 0;
+        int parent = -1;
+        solveBydfs(src,parent,timing,insTime,lowInsTime,visited,adj,bridges);
+        return bridges;
+        
+
+    }
+};
