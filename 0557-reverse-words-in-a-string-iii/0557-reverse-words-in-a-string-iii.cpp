51bfda9aea89c9259fb184671fffa89150edb90d
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-10-02 00:19:06 +0530

    Time: 17 ms (46.37%), Space: 11.7 MB (36.76%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp ="";
+        string ans ="";
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i] != ' ')
+            {
+                temp += s[i];
+            }
+            else
+            {
+               reverse(temp.begin(),temp.end());
+               ans += temp;
+                ans+=' ';
+
+                temp="";
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        ans += temp;
+        return ans;
+    }
+};
