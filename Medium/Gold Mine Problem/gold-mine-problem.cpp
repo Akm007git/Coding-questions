2fb9d823fca5402e5e9abb2de49292bb50d95d83
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2023-12-14 14:50:41 +0530

    Updated Solution - GfG to GitHub

--- Medium/Gold Mine Problem/gold-mine-problem.cpp
+++ Medium/Gold Mine Problem/gold-mine-problem.cpp
@@ -7,34 +7,38 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-private:
- int fun(vector<vector<int>> &M,int n,int m,int i,int j,vector<vector<int>>&dp)
-    {
-        if(i<0 || i>=n || j>=m)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-            
-        int n1=fun(M,n,m,i-1,j+1,dp);
-        int n2=fun(M,n,m,i,j+1,dp);
-        int n3=fun(M,n,m,i+1,j+1,dp);
-        
-        return dp[i][j]=M[i][j]+max(max(n1,n2),n3);
-    }
-    
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
 public:
-    int maxGold(int n, int m, vector<vector<int>> M)
-    {
-        // code here
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            int k=fun(M,n,m,i,0,dp);
-            if(k>ans)
-                ans=k;
+    int solve(int r, int c, vector<vector<int>>& arr, int n, int m,vector<vector<int>>&dp) {
+        // base case
+        if (r < 0 || c < 0 || r >= n || c >= m) {
+            return 0;
         }
+        
+        if(dp[r][c] != -1){
+            return dp[r][c];
+        }
+        int currentGold = arr[r][c];
+        int dUpRight = solve(r - 1, c + 1, arr, n, m,dp);
+        int dDownRight = solve(r + 1, c + 1, arr, n, m,dp);
+        int right = solve(r, c+1 , arr, n, m,dp);
+
+        dp[r][c] = currentGold + max(dUpRight, max(dDownRight, right));
+        return dp[r][c];
+    }
+
+    int maxGold(int n, int m, vector<vector<int>> M) {
+        // finding the max value from the first column
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+           int val =  solve(i, 0, M, n, m, dp);
+           ans = max(val, ans);
+        }
+        
         return ans;
     }
 };
