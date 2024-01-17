4ad0c8d7c325390413effe02977663b00e2efe21
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-01-17 19:46:56 +0530

    Added Solution - GfG to GitHub

---
+++ School/Find difference between sum of diagonals/find-difference-between-sum-of-diagonals.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int diagonalSumDifference(int N, vector<vector<int>> Grid) {
+        // code here
+        int n = Grid.size();
+        int leftSum = 0,rightSum = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            leftSum += Grid[i][i];
+            rightSum += Grid[i][n-i-1];
+        }
+        
+        return abs(rightSum-leftSum);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> Grid1(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> Grid1[i][j];
+        Solution ob;
+        cout << ob.diagonalSumDifference(N, Grid1) << "\n";
+    }
+}
+// } Driver Code Ends
