e698c2b35c804cf4c5d64ae7fbd5e5e7f2484f10
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-03-22 23:33:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -5,56 +5,53 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     private:
-    bool bfs(int start, vector<int>adj[], vector<bool>&visited)
+    bool bfs(int start,vector<int>adj[],vector<bool>&visited)
     {
+        queue<pair<int,int>>q;
         
-        visited[start] = true; // initially start marked as true;
-        queue< pair <int,int > >q;
-        
-        q.push(make_pair(start,-1)); // initially parent is passing as -1
+        q.push({start,-1});
+        visited[start] = true;
         
         while(!q.empty())
         {
             int node = q.front().first;
             int parent = q.front().second;
-            
             q.pop();
             
-            for(auto it: adj[node])
+            for(auto it:adj[node])
             {
-                if(!visited[it]){ // if the adj nodes (it) not visited, pusg in queue, and visit
-                    q.push({it,node}); // that case parent will node, & "it" is adj nodes
+                if(!visited[it])
+                {
+                    q.push({it,node});
                     visited[it] = true;
                 }
-                else if( it != parent ){ // menas, adj node visited and also not parent, means cycle must
+                else if(it != parent && visited[it])
+                {
                     return true;
                 }
             }
         }
-        // if any case doesnt follow, means,no cycle
         return false;
     }
   public:
     // Function to detect cycle in an undirected graph.
-    
-    // USING BFS----------------
-    
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<bool>visited(V,false);
-        
-        
-        for(int i=0;i<V;i++)
-        {
-            if(!visited[i])
-            {
-                if(bfs(i,adj,visited)){
-                     return true;
+       
+      
+       for(int i = 0;i<V;i++)
+       {
+           if(!visited[i])
+           {
+                if(bfs(i,adj,visited))
+                {
+                    return true;
                 }
-                
-            }
-        }
-        return false;
+           }
+       }
+       return false;
+        
     }
 };
 
