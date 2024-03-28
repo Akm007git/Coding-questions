49c1786bd12df11049e8ad913a09a251310be3a7
Anup Kumar Mahata <109021175+Akm007git@users.noreply.github.com>
2024-03-28 21:35:48 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Insert in Middle of Linked List/insert-in-middle-of-linked-list.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node 
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// function to display the linked list
+void display(Node* head)
+{
+	while (head != NULL) {
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout<<endl;
+}
+
+Node* insertInMiddle(Node* head, int x);
+
+int main()
+{
+    int T, n, x;
+    cin>>T;
+    while(T--)
+    {
+        cin>> n >> x;
+        
+        struct Node *head = new Node(x);
+        struct Node *tail = head;
+        
+        for (int i=0; i<n-1; i++)
+        {
+            cin>>x;
+            tail->next = new Node(x);
+            tail = tail->next;
+        }
+        
+        cin>> x;
+        head = insertInMiddle(head, x);
+        display(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to insert a node in the middle of the linked list.
+
+
+Node* insertInMiddle(Node* head, int x) {
+    if (head == NULL) {
+        head = new Node(x);
+        return head;
+    }
+
+    Node* temp = head;
+    int count = 0;
+    
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    
+    temp = head;
+    int pos = 1;
+
+    while (temp != NULL && pos < (count+1) / 2) {
+        temp = temp->next;
+        pos++;
+    }
+
+   
+	Node* newNode = new Node(x); 
+
+     	Node* _next = temp->next;
+
+		temp->next = newNode;
+			newNode->next = _next;
+
+    return head;
+}
